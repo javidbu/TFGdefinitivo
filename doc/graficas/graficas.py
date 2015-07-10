fd1e1ad90d38dbf7032a9d0001fb7ab1eaf65b19
Javi <javidbu@gmail.com>
2015-07-10 16:15:01 +0200

    Añadidas graficas para el Random Forest
    Pues eso...

--- doc/graficas/graficas.py
+++ doc/graficas/graficas.py
@@ -1,8 +1,10 @@
 from numpy import linspace, exp, log
 from matplotlib.pyplot import plot, figure, grid, xlabel, ylabel, legend, show, savefig
 
+#Tamanho fuente
 fsize = 20
 
+#Valores graficas
 xsigmoid = linspace(-5,5,200)
 ysigmoid = 1/(1+exp(-xsigmoid))
 
@@ -10,6 +12,11 @@ xerr = linspace(0,1,100)
 yerr1 = -log(xerr)
 yerr0 = -log(1-xerr)
 
+xDT = linspace(0,1,100)
+yent = -xDT*log(xDT) - (1-xDT)*log(1-xDT)
+yGini = 2*xDT*(1-xDT)
+
+#Graficas
 figure()
 plot(xsigmoid, ysigmoid, linewidth=2)
 grid()
@@ -26,5 +33,14 @@ ylabel(r"$J(\theta)$",fontsize=fsize)
 legend(loc=9,fontsize=fsize)
 
 savefig('C:/Users/Javi/Desktop/TFGdefinitivo/doc/graficas/costLogReg.ps')
+
+figure()
+plot(xDT, yent, label='entropia', linewidth=2)
+plot(xDT, yGini, label='Gini', linewidth=2)
+xlabel(r"$p_1$", fontsize=fsize)
+ylabel('criterio', fontsize = fsize)
+legend(loc=8, fontsize=fsize)
+
+savefig('C:/Users/Javi/Desktop/TFGdefinitivo/doc/graficas/DecTreeCriterios.ps')
 
 #show()
